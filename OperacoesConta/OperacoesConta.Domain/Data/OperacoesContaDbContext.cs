cde2c367d9397c3a0cebc5bf8d2bf7157d30cca2
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2021-04-16 18:30:45 -0300

    Inclusão de pacote EntityFrameworkCore In Memory e criação de classe OperacoesContaDbContext

---
+++ OperacoesConta/OperacoesConta.Domain/Data/OperacoesContaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OperacoesConta.Domain.Entities;
+
+namespace OperacoesConta.Domain.Data
+{
+    public class OperacoesContaDbContext : DbContext
+    {
+        public DbSet<Conta> Contas { get; set; }
+
+        public OperacoesContaDbContext(DbContextOptions<OperacoesContaDbContext> options) 
+            : base(options) { }
+    }
+}
