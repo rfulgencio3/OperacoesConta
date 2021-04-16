c5c508701c3be0f44e383f963cd6d307cd6b8379
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2021-04-16 18:40:48 -0300

    Implementação de Repository e Interface de Conta

---
+++ OperacoesConta/OperacoesConta.Domain/Interfaces/IContaRepository.cs
@@ -0,0 +1,13 @@
+﻿using OperacoesConta.Domain.Entities;
+using System.Collections.Generic;
+
+namespace OperacoesConta.Domain.Interfaces
+{
+    public interface IContaRepository
+    {
+        void Adicionar(Conta conta);
+        IEnumerable<Conta> ObterTodas();
+        IEnumerable<Conta> ObterPorIdentificador(int identifier);
+        IEnumerable<Conta> ObterPorNome(string name);
+    }
+}
---
+++ OperacoesConta/OperacoesConta.Domain/Repository/ContaRepository.cs
@@ -0,0 +1,38 @@
+﻿using OperacoesConta.Domain.Data;
+using OperacoesConta.Domain.Entities;
+using OperacoesConta.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OperacoesConta.Domain.Repository 
+{
+    public class ContaRepository : IContaRepository
+    {
+        private readonly OperacoesContaDbContext _operacoesContaDbContext;
+        public ContaRepository(OperacoesContaDbContext operacoesContaDbContext)
+        {
+            _operacoesContaDbContext = operacoesContaDbContext;
+        }
+        public void Adicionar(Conta conta)
+        {
+            _operacoesContaDbContext.Contas.Add(conta);
+            _operacoesContaDbContext.SaveChanges();
+        }
+        public IEnumerable<Conta> ObterTodas()
+        {
+            return _operacoesContaDbContext.Contas.ToList();
+        }
+        public IEnumerable<Conta> ObterPorIdentificador(int identifier)
+        {
+            return _operacoesContaDbContext.Contas
+                .Where(p => p.Identifier.Equals(identifier))
+                .ToList();
+        }
+        public IEnumerable<Conta> ObterPorNome(string name)
+        {
+            return _operacoesContaDbContext.Contas
+                .Where(p => p.Identifier.Equals(name))
+                .ToList();
+        }
+    }
+}
