fdf3d21a71e3c99d9565614d18c13976ffac8765
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2021-04-16 18:22:55 -0300

    Criação de casos de teste e refactoring em Conta

---
+++ OperacoesConta/OperacoesConta.Domain.Tests/Contas/ContaTests.cs
@@ -0,0 +1,34 @@
+﻿using OperacoesConta.Domain.Entities;
+using System;
+using Xunit;
+
+namespace OperacoesConta.Domain.Tests.Contas
+{
+    public class ContaTests
+    {
+        [Fact(DisplayName = "Validar Informações de Criação de Conta")]
+        [Trait("Categoria", "Conta - Criar Conta")]
+        public void Conta_Criar_ValidarInformacoes()
+        {
+            // Arrange && Act
+            var novaConta = new Conta(1, "Conta do Fulgencio", "Conta Salário", "ACTIVE");
+
+            // Assert
+            Assert.Equal(1, novaConta.Identifier);
+            Assert.Equal("Conta do Fulgencio", novaConta.Name);
+            Assert.Equal("Conta Salário", novaConta.Description);
+            Assert.Equal("ACTIVE", novaConta.Status);
+        }
+
+        [Fact(DisplayName = "Retornar Erro de Criação de Conta sem Nome")]
+        [Trait("Categoria", "Conta - Criar Conta")]
+        public void Conta_VerificarContaSemNome_DeveRetornarException()
+        {
+            // Arrange && Act
+            var novaConta = new Conta(2, "", "Conta Salário", "ACTIVE");
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => novaConta.ValidarNome(novaConta));
+        }
+    }
+}
--- OperacoesConta/OperacoesConta.Domain/Entities/Conta.cs
+++ OperacoesConta/OperacoesConta.Domain/Entities/Conta.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace OperacoesConta.Domain.Entities
 {
-    class Conta
+    public class Conta
     {
+        public int Identifier { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public Conta(int identifier, string name, string description, string status)
+        {
+            Identifier = identifier;
+            Name = name;
+            Description = description;
+            Status = status;
+        }
+
+        public void ValidarNome(Conta conta)
+        {
+            if (string.IsNullOrEmpty(conta.Name.ToString()))
+            {
+                throw new ArgumentException("Nome de conta não encontrado.");
+            }
+        }
     }
 }
